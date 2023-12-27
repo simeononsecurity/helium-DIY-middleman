ba807fcf08a1c78faf7afb4762b1c956875725c6
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-12-26 21:42:18 -0600

    PoC Still Works on Crankk

--- gateways2miners.py
+++ gateways2miners.py
@@ -286,8 +286,8 @@ class GW2Miner:
 
 
 def packet_is_poc_challenge(rxpk: dict):
-    return rxpk.get('size') == 52 and rxpk.get('datr') == 'SF9BW125'
-
+    return (rxpk.get('size') == 52 and rxpk.get('datr') == 'SF9BW125') or rxpk.get('size') == 21
+    #return (rxpk.get('size') == 52 ) and rxpk.get('datr') == 'SF9BW125'
 
 def configure_logger(debug=False):
     # setup logger
